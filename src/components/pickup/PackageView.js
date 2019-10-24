42f64dd0d06b3eee701402adbeddcd9dd428071d
JEDIDIAH YSAMM CAYCHINGCO (SHP-IRIS-ISD-OOCLL/MNL) <CAYCHJE@OOCL.COM>
2019-10-24 12:17:39 +0800

    Connect Add package to backend

--- src/components/pickup/PackageView.js
+++ src/components/pickup/PackageView.js
@@ -1,18 +1,71 @@
 import React from 'react';
 
-class PackageView extends React.Component{
+class PackageView extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {packageNumber: "", receiver: "", phoneNumber: "", weight: ""}
+    }
+
+    postNewPackage = () => {
+        const newPackage = {
+            packageNumber: this.state.packageNumber,
+            receiver: this.state.receiver,
+            phoneNumber: this.state.phoneNumber,
+            weight: this.state.weight
+        };
+
+        fetch("http://localhost:8080/packages", {
+            mode: 'cors',
+            method: 'POST',
+            headers: new Headers({'Content-Type': 'application/json'}),
+            body: JSON.stringify(newPackage)
+        })
+            .catch(e => {
+                console.log(e);
+            });
+    };
+
+    updatePackageNumber = (e) => {
+        this.setState({packageNumber: e.target.value});
+    };
+
+    updateReceiver = (e) => {
+        this.setState({receiver: e.target.value});
+    };
+
+    updatePhoneNumber = (e) => {
+        this.setState({phoneNumber: e.target.value});
+    };
+
+    updateWeight = (e) => {
+        this.setState({weight: e.target.value});
+    };
+
     render() {
         return (
             <div>
                 <h1>New package item</h1>
                 <div>
-                    <div><span>PackageNumber</span><input type="text"/></div>
-                    <div><span>Receiver</span><input type="text"/></div>
-                    <div><span>Phone Number</span><input type="text"/></div>
-                    <div><span>Weight</span><input type="text"/></div>
+                    <div>
+                        <span>PackageNumber</span>
+                        <input type="text" value={this.state.packageNumber} onChange={this.updatePackageNumber}/>
+                    </div>
+                    <div>
+                        <span>Receiver</span>
+                        <input type="text" value={this.state.receiver} onChange={this.updateReceiver}/>
+                    </div>
+                    <div>
+                        <span>Phone Number</span>
+                        <input type="text" value={this.state.phoneNumber} onChange={this.updatePhoneNumber}/>
+                    </div>
+                    <div>
+                        <span>Weight</span>
+                        <input type="text" value={this.state.weight} onChange={this.updateWeight}/>
+                    </div>
                 </div>
                 <div>
-                    <button onClick={this.postNewPickup}>Confirm</button>
+                    <button onClick={this.postNewPackage}>Confirm</button>
                     <button>Cancel</button>
                 </div>
             </div>
